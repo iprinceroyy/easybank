e923d4962ba4296ffc84647b2ca78b74312d2e26
Prince Roy <princeku.ray143@gmail.com>
2022-04-13 23:21:10 +0530

    desktop view navbar

--- js/script.js
+++ js/script.js
@@ -5,9 +5,20 @@ const navMenu = document.querySelector(".nav-menu");
 let navIsToggled = false;
 
 btn.addEventListener('click', () => {
-    icon.src = "assets/images/icon-close.svg";
-    navMenu.classList.toggle("show");
 
+    if (!navIsToggled) {
+        icon.src = "assets/images/icon-close.svg";
+        navMenu.classList.add("show");
+    } else {
+        navMenu.classList.remove("show");
+        icon.src = "assets/images/icon-hamburger.svg";
+    }
 
     navIsToggled = navIsToggled ? false : true;
+});
+
+window.addEventListener('resize', function() {
+    if (this.innerWidth > 992) {
+        this.document.body.style.overflowY = 'auto';
+    }
 });
